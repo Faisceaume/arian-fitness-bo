9f0b457712a7730b3d2df0d60443a00daa8a4903
Assamoi-Intelligence <juniasdaniel05@gmail.com>
2020-02-11 10:54:23 +0000

    serie fixe

--- src/app/exercices/exercices-series/exercices-series.component.ts
+++ src/app/exercices/exercices-series/exercices-series.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import  { PathologiesService } from '../../shared/pathologies/pathologies.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { PathologiesService } from '../../shared/pathologies/pathologies.service';
 import { ObjectifsService } from 'src/app/shared/objectifs/objectifs.service';
 import { ExercicesService } from '../exercices.service';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import { Exercice } from '../exercice';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-exercices-series',
@@ -22,17 +25,30 @@ export class ExercicesSeriesComponent implements OnInit {
   pathologies: any[];
   objectifs: any[];
   dataSource: MatTableDataSource<any>;
-  displayedColumns: string[] = ['nom','timestamp', 'objectifJour', 'idPathologie', 'action'];
+  exerciceSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ['nom', 'timestamp', 'senior', 'objectifJour', 'idPathologie', 'action'];
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   dataIsLoad = false;
   selectExercices = false;
+  exerciceList: any[];
+  filteredExercice: Observable<any[]>;
+  exerciceForm: FormGroup;
+  exerciceAdded: any[];
+  exerciceAddLenght: number;
+  isDisplayFormExo = false;
+  isFromEdit = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private pathologiesService: PathologiesService,
     private objectifService: ObjectifsService,
     private exerciceService: ExercicesService
-    ) { }
+    ) {
+      /*this.filteredExercice = this.exerciceCtrl.valueChanges.pipe(
+        startWith(''),
+        map(exercice => exercice ? this._filterExercices(exercice) : this.exerciceList.slice() )
+      );*/
+     }
 
   ngOnInit() {
 
@@ -59,6 +75,7 @@ export class ExercicesSeriesComponent implements OnInit {
   initForm() {
     this.serieFormAdd = this.formBuilder.group({
       nom: ['', Validators.required],
+      senior: ['', Validators.required],
       objectifJour: [this.objectifs, Validators.required],
       idPathologie: [this.pathologies, Validators.required]
     });
@@ -67,10 +84,67 @@ export class ExercicesSeriesComponent implements OnInit {
 
   /* ADD, EDIT, DELETE */
   onSubmit() {
-    const data = this.serieFormAdd.value;
-    this.exerciceService.createSerieExercice(data);
-    this.initForm();
+    this.exerciceForm = this.formBuilder.group({
+      exercice: ['', [Validators.required] ]
+    });
+    this.selectExercices = true;
+    this.isdisplayedAdd = false;
+    this.exerciceService.getAllExercices();
+    this.exerciceService.exerciceSubject.subscribe(data => {
+      this.exerciceSource = new MatTableDataSource( data );
+      this.exerciceList = this.exerciceSource.filteredData;
+      this.isDisplayFormExo = true;
+      this.exerciceAdded = [];
+      this.exerciceAddLenght = this.exerciceAdded.length;
+    });
   }
+
+  displayFn(exercice: any): string {
+    return exercice.nom;
+  }
+
+  applyFilter(filterValue: string) {
+    this.exerciceSource.filter = filterValue.trim().toLowerCase();
+    this.exerciceList = this.exerciceSource.filteredData;
+  }
+
+  onAddExercice() {
+    const ex =  this.exerciceForm.get('exercice').value;
+    this.exerciceAdded.push( ex );
+    this.exerciceAddLenght = this.exerciceAdded.length;
+    this.exerciceForm = this.formBuilder.group({
+      exercice: ['', Validators.required]
+    });
+  }
+
+  clearExo(i) {
+    this.exerciceAdded.splice(i, 1);
+    this.exerciceAddLenght = this.exerciceAdded.length;
+  }
+
+  onAddSerieFixe() {
+    if ( this.isFromEdit ) {
+      const data1 = this.serieFormEdit.value;
+      const data2 = this.exerciceAdded;
+      const data = Object.assign(data1, {exercice: data2});
+      this.exerciceService.updateSerieExerciceFixe(this.idOneSerie, data);
+      this.selectExercices = false;
+      this.isDispalayList = true;
+      this.isFromEdit = false;
+    } else {
+      const data1  = this.serieFormAdd.value;
+      const data2 = this.exerciceAdded;
+      const data = Object.assign(data1, {exercice: data2});
+      this.exerciceService.createSerieExercice(data);
+      this.returnToAdd();
+      this.initForm();
+    }
+  }
+
+  /*private _filterExercices(value: string): any[] {
+    const filterValue = value.toLowerCase();
+    return this.exerciceList.filter(exercice => exercice.consignecourte.toLowerCase().indexOf(filterValue) === 0);
+  }*/
 
   onEdit( id ) {
     this.isDisplayEdit = true;
@@ -81,10 +155,13 @@ export class ExercicesSeriesComponent implements OnInit {
       console.log( data );
       this.serieFormEdit = this.formBuilder.group({
         nom: [data.nom, Validators.required],
+        senior: [data.senior, Validators.required],
         objectifJour: [data.objectifJour, Validators.required],
         idPathologie: [data.idPathologie, Validators.required]
       });
       this.dataIsLoad = true;
+      this.exerciceAdded = data.exercice;
+      this.exerciceAddLenght = this.exerciceAdded.length;
     });
   }
 
@@ -93,9 +170,21 @@ export class ExercicesSeriesComponent implements OnInit {
   }
 
   onUpdate() {
-    const data = this.serieFormEdit.value;
+    this.exerciceForm = this.formBuilder.group({
+      exercice: ['', [Validators.required] ]
+    });
+    this.selectExercices = true;
+    this.isDisplayEdit = false;
+    this.isFromEdit = true;
+    this.exerciceService.getAllExercices();
+    this.exerciceService.exerciceSubject.subscribe( data => {
+      this.exerciceSource = new MatTableDataSource( data );
+      this.exerciceList = this.exerciceSource.filteredData;
+      this.isDisplayFormExo = true;
+    });
+    /*const data = this.serieFormEdit.value;
     this.exerciceService.updateSerieExerciceFixe(this.idOneSerie, data);
-    this.hiddenEdit();
+    this.hiddenEdit();*/
   }
 
 
@@ -111,6 +200,16 @@ export class ExercicesSeriesComponent implements OnInit {
   hiddenEdit() {
     this.isDisplayEdit = this.isdisplayedAdd = false;
     this.isDispalayList = true;
+  }
+  returnToAdd() {
+    if ( this.isFromEdit ) {
+      this.isDisplayEdit = true;
+      this.selectExercices = this.isFromEdit = false;
+    } else {
+    this.isdisplayedAdd = true;
+    this.selectExercices = false;
+    this.exerciceAdded = [];
+    }
   }
 
 }
