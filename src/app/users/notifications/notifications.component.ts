80cc26a9d9c568ed5dee06f7ceb6851a5d0314a2
Assamoi-Intelligence <56390939+Assamoi-Intelligence@users.noreply.github.com>
2020-08-27 14:12:00 +0000

    update notification

--- src/app/users/notifications/notifications.component.ts
+++ src/app/users/notifications/notifications.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { NotificationsService } from './notifications.service';
 import { MatDialogConfig, MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatTableDataSource, MatSort } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-notifications',
@@ -10,18 +11,24 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class NotificationsComponent implements OnInit {
 
-  displayedColumns: string[] = ['title', 'content', 'status', 'timestamp', 'action'];
+  displayedColumns: string[] = ['title', 'content', 'status', 'timestamp', 'user', 'action'];
   dataSource: MatTableDataSource<any>;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  users: any[];
 
   constructor(
     private notificationsService: NotificationsService,
-    private matDialog: MatDialog
+    private matDialog: MatDialog,
+    private userService: UsersService
   ) { }
 
   ngOnInit() {
     this.notificationsService.getAllNotifications();
+    this.userService.getAllUsers();
     this.notificationsService.notificationsSubject.subscribe(data => {
+      this.userService.userSubject.subscribe((data: any[]) => {
+        this.users = data.map(d => { return  {id: d.id, nom: d.nom, prenom: d.prenom}});
+      });
       this.dataSource = new MatTableDataSource( data );
       this.sort = this.dataSource.sort;
     });
@@ -31,7 +38,7 @@ export class NotificationsComponent implements OnInit {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.width = '50%';
-    dialogConfig.data = {create: true, update: false};
+    dialogConfig.data = {create: true, update: false, users: this.users};
     this.matDialog.open(DialogNotificationComponent, dialogConfig);
   }
 
@@ -39,7 +46,7 @@ export class NotificationsComponent implements OnInit {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.width = '50%';
-    dialogConfig.data = {create: false, update: true, key: id};
+    dialogConfig.data = {create: false, update: true, key: id, users: this.users};
     this.matDialog.open(DialogNotificationComponent, dialogConfig);
   }
 
@@ -74,26 +81,31 @@ export class DialogNotificationComponent implements OnInit {
 
   form: FormGroup;
   isLoadData = false;
+  users: any[];
+  userSelected: any;
 
   constructor(
     private notificationsService: NotificationsService,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private formBuilder: FormBuilder,
-    private dialogRef: MatDialogRef<DialogNotificationComponent>
+    private dialogRef: MatDialogRef<DialogNotificationComponent>,
+    private userService: UsersService
     ) {}
 
   ngOnInit() {
-
     if( this.data.create ) {
       this.initForm();
     }
     if ( this.data.update ) {
+      this.initForm();
       this.notificationsService.getOneNotification(this.data.key);
       this.notificationsService.notificationOneSubject.subscribe(data => {
-        this.form = this.formBuilder.group({
-          title: [data.title, Validators.required],
-          content: [data.content, Validators.required],
-          status: [data.status, Validators.required]
+        this.userSelected = data.user;
+        this.form.patchValue({
+          title: data.title,
+          content: data.content,
+          status: data.status,
+          user: {id: data.user, nom: data.user.nom, prenom: data.user.prenom}
         });
         this.isLoadData = true;
       });
@@ -104,6 +116,7 @@ export class DialogNotificationComponent implements OnInit {
     this.form = this.formBuilder.group({
       title: ['', Validators.required],
       content: ['', Validators.required],
+      user: ['', Validators.required],
       status: ['', Validators.required]
     });
   }
