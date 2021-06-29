fc9530c1f3ee232ff30fe8f349a7a333b3fe3d34
Assamoi-Intelligence <56390939+Assamoi-Intelligence@users.noreply.github.com>
2021-06-29 13:39:10 +0200

    video suppress

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -89,10 +89,9 @@ export class SharedService {
     }
 
     deleteFile(url: string, urlThumbnail?: string, typeFile?: string) {
-      let storageRef: any = '';
-      let storageRefThumbnail: any = '';
-      console.log(urlThumbnail, typeFile);
+      let storageRef: any = '';      
       if(urlThumbnail) {
+        let storageRefThumbnail: any = '';
         try {
           storageRef =  this.store.storage.refFromURL(url);
           storageRefThumbnail =  this.store.storage.refFromURL(urlThumbnail);
@@ -114,6 +113,7 @@ export class SharedService {
         }
       } else {
         try {
+          console.log(url);
           storageRef =  this.store.storage.refFromURL(url);
         } catch (error) {
         }
